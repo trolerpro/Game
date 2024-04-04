12cade1a0b4e2971feacaf1fb7e0529cb2140f41
trolerpro <152172228+trolerpro@users.noreply.github.com>
2024-04-05 00:03:17 +0300

    Add files via upload

--- Spele_aivars.py
+++ Spele_aivars.py
@@ -1,11 +1,23 @@
-import random, colorama
+from random import randint
 
-from colorama import Fore
-
-print(random.randint(1, 100))
-
-print(Fore.RED + 'Mazāk')
-
-print(Fore.BLUE + 'Vairāk')
-
-print(Fore.GREEN + 'Pareizi')
+from colorama import Fore #pip instal colorama terminālī lai strādātu krāsas
+meginajumi = 10
+cipars = randint(1, 100)
+print(Fore.MAGENTA + "Mini skaitli no 1 līdz 100")
+while True:
+    
+    guess = input(Fore.WHITE + "ievadi savu minējumu (skaitli!!!) ")
+        
+    if int(guess) > cipars:
+        print(Fore.CYAN + 'Mazāk')
+        meginajumi = meginajumi - 1
+    elif int(guess) < cipars:
+        print(Fore.BLUE + 'Vairāk')
+        meginajumi = meginajumi - 1
+    elif int(guess) == cipars:
+        print(Fore.LIGHTGREEN_EX + 'Pareizi')
+        break
+    
+    if meginajumi == 0:
+        print(Fore.RED + "Beidzās minējumi :(")
+        break
