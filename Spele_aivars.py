8064a53e969ddfc8afc2e6c7d42878e67ed2467f
trolerpro <152172228+trolerpro@users.noreply.github.com>
2024-04-05 21:49:13 +0300

    Spele_aivars.py

--- Spele_aivars.py
+++ Spele_aivars.py
@@ -1,12 +1,28 @@
 from random import randint
 
-from colorama import Fore #pip install colorama terminālī lai strādātu krāsas
-meginajumi = 10
-cipars = randint(1, 100)
-print(Fore.MAGENTA + "Mini skaitli no 1 līdz 100")
+from colorama import Fore #pip instal colorama terminālī lai strādātu krāsas
+
+
+level = input(Fore.YELLOW + "izvēlies grūtību (viegla) vai (grūta) un ieraksti: ")#lai korekti izvēlētos grūtību jāierakstā tāpat kā rakstīts iekavās
+
+if level.lower() == 'viegla':
+    meginajumi = 8
+    cipars = randint(1, 100)
+    print(Fore.MAGENTA + "mini skaitli no 1 līdz 100:")
+elif level.lower() == 'grūta':
+    meginajumi = 16
+    cipars = randint(1, 1000)
+    print(Fore.MAGENTA + "mini skaitli no 1 līdz 1000:")
+else:
+    print(Fore.MAGENTA + "Tā kā tu nepareizi ierakstīji grūtību tātad es tev došu vieglo līmeni")
+    meginajumi = 8
+    cipars = randint(1, 100)
+    print(Fore.MAGENTA + "mini skaitli no 1 līdz 100:")
+
+
 while True:
     
-    guess = input(Fore.WHITE + "ievadi savu minējumu (skaitli!!!) ")
+    guess = input(Fore.WHITE + "ievadi savu minējumu (skaitli!): ")
         
     if int(guess) > cipars:
         print(Fore.CYAN + 'Mazāk')
@@ -19,5 +35,5 @@ while True:
         break
     
     if meginajumi == 0:
-        print(Fore.RED + "Beidzās minējumi :(")
+        print(Fore.RED + "Beidzās minējumi :(, skaitlis bija", cipars)
         break
